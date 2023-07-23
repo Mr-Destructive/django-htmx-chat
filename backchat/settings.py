ebb4b296ba8445e01a596e85a8839deb1593e256
mr_destructive <gormeet711@gmail.com>
2023-07-23 16:19:53 +0530

    feat: add static files

--- backchat/settings.py
+++ backchat/settings.py
@@ -86,6 +86,10 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
