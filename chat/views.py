13fc73bc7a93d6bdab511383c2422496a63f0b1e
rense <rense@me.com>
2023-12-31 20:37:21 +0100

    Don't crash but throw a nice error when room-name can't be found

--- chat/views.py
+++ chat/views.py
@@ -16,21 +16,28 @@ def index(request, slug):
 
 @login_required
 def room_create(request):
-    if request.method == "POST":
-        room_name = request.POST["room_name"]
-        uid = str("".join(random.choices(string.ascii_letters + string.digits, k=4)))
-        room_slug = slugify(room_name + "_" + uid)
-        room = Room.objects.create(name=room_name, slug=room_slug)
-        return redirect(reverse("chat", kwargs={"slug": room.slug}))
-    else:
+    if request.method != "POST":
         return render(request, "chat/create.html")
+
+    room_name = request.POST["room_name"]
+    uid = str("".join(random.choices(string.ascii_letters + string.digits, k=4)))
+    room_slug = slugify(room_name + "_" + uid)
+    room = Room.objects.create(name=room_name, slug=room_slug)
+    return redirect(reverse("chat", kwargs={"slug": room.slug}))
 
 
 @login_required
 def room_join(request):
-    if request.method == "POST":
-        room_name = request.POST["room_name"]
-        room = Room.objects.get(slug=room_name)
-        return redirect(reverse("chat", kwargs={"slug": room.slug}))
-    else:
+    if request.method != "POST":
         return render(request, "chat/join.html")
+
+    room_name = request.POST["room_name"]
+    try:
+        room = Room.objects.get(slug=room_name)
+    except Room.DoesNotExist:
+        return render(
+            request,
+            "chat/join.html",
+            {"error": "This room does not exist. Please check the name and try again."},
+        )
+    return redirect(reverse("chat", kwargs={"slug": room.slug}))
